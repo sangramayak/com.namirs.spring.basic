bba6ff141a062baf440aba2699e46846b2b5c54e
srikanta <srikanta.ydon@gmail.com>
2018-02-05 14:46:48 +0530

    Its from Sekhar sir mixxed

---
+++ LockAppUser2/src/Client2.java
@@ -0,0 +1,29 @@
+import javax.swing.JOptionPane;
+
+import org.hibernate.LockOptions;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class Client2 {
+	public static void main(String...args){
+		Configuration conf= new Configuration();
+		conf .configure("hibernate.cfg.xml");
+		ServiceRegistry registry=new StandardServiceRegistryBuilder().applySettings(conf.getProperties()).build();
+		SessionFactory factory=conf.buildSessionFactory(registry);
+		Session session=factory.openSession();
+		Transaction tx=session.beginTransaction();
+		//Student s=(Student)session.get(Student.class,101);
+		Student s=(Student)session.get(Student.class,101,LockOptions.UPGRADE);//pessimistic lock
+		
+		String str=JOptionPane.showInputDialog("Enter new Marks(user2)");
+		int m=Integer.parseInt(str);
+		s.setMarks(m);
+		tx.commit();
+		session.close();
+		factory.close();
+	}
+}
